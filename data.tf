8918abdb705f9910e1c8441495da65a0cdd84f13
Mukesh Sharma <mukeshsharma24@gmail.com>
2022-03-29 11:52:21 +1300

    Added data lookup resources

---
+++ data.tf
@@ -0,0 +1,11 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+
+# lookup latest AL2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+  name_regex  = "amzn2-ami-hvm*"
+}
