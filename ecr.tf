c3cdcfec79c3d138539e910923cdc4f316d8240f
Mukesh Sharma <mukeshsharma24@gmail.com>
2022-03-29 15:19:08 +1300

    Added ecs cluster and ecr config

---
+++ ecr.tf
@@ -0,0 +1,10 @@
+################################################################################
+resource "aws_ecr_repository" "n8n_ecr" {
+  name                 = "${var.environment}-n8n-ecr"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
