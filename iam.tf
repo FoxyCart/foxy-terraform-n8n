6f3146a4f2e3077a08064529554647e819fcbcd8
Mukesh Sharma <mukeshsharma24@gmail.com>
2022-04-13 10:29:55 +1200

    Added codebuild resources to build n8n container and push to ecr

---
+++ iam.tf
@@ -0,0 +1,100 @@
+#--------------------------------------------------------------------------
+# IAM role
+#--------------------------------------------------------------------------
+module "codebuild_admin_iam_assumable_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "~> 4.5"
+
+  trusted_role_services = [
+    "codebuild.amazonaws.com"
+  ]
+
+  role_requires_mfa       = false
+  create_role             = true
+  create_instance_profile = true
+
+  role_name = "codebuild-admin-role-${random_id.random_id.hex}"
+
+  custom_role_policy_arns = [
+    "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess",
+    "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess",
+    module.codebuild_ecr_iam_policy.arn
+  ]
+}
+
+
+
+module "codebuild_ecr_iam_policy" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "~> 4.5"
+
+  name        = "codebuild_ecr_iam_policy_${random_id.random_id.hex}"
+  path        = "/"
+  description = "My example policy"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "ecr:PutImageTagMutability",
+                "ecr:StartImageScan",
+                "ecr:DescribeImageReplicationStatus",
+                "ecr:ListTagsForResource",
+                "ecr:UploadLayerPart",
+                "ecr:BatchDeleteImage",
+                "ecr:ListImages",
+                "ecr:BatchGetRepositoryScanningConfiguration",
+                "ecr:DeleteRepository",
+                "ecr:CompleteLayerUpload",
+                "ecr:TagResource",
+                "ecr:DescribeRepositories",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:ReplicateImage",
+                "ecr:GetLifecyclePolicy",
+                "ecr:PutLifecyclePolicy",
+                "ecr:DescribeImageScanFindings",
+                "ecr:GetLifecyclePolicyPreview",
+                "ecr:PutImageScanningConfiguration",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:DeleteLifecyclePolicy",
+                "ecr:PutImage",
+                "ecr:UntagResource",
+                "ecr:BatchGetImage",
+                "ecr:DescribeImages",
+                "ecr:StartLifecyclePolicyPreview",
+                "ecr:InitiateLayerUpload",
+                "ecr:GetRepositoryPolicy"
+            ],
+            "Resource": "arn:aws:ecr:*:185203724531:repository/*"
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetRegistryPolicy",
+                "ecr:BatchImportUpstreamImage",
+                "ecr:CreateRepository",
+                "ecr:DescribeRegistry",
+                "ecr:DescribePullThroughCacheRules",
+                "ecr:GetAuthorizationToken",
+                "ecr:PutRegistryScanningConfiguration",
+                "ecr:CreatePullThroughCacheRule",
+                "ecr:DeletePullThroughCacheRule",
+                "ecr:GetRegistryScanningConfiguration",
+                "ecr:PutReplicationConfiguration"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+
+  tags = {
+    PolicyDescription = "Policy created using heredoc policy"
+  }
+}
